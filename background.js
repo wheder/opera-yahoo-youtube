9f750ce5531805671a17d2acc2e7e89527500e43
wheder <wheder@gmail.com>
2015-09-30 13:12:38 +0200

    init

---
+++ background.js
@@ -0,0 +1,15 @@
+var host = "https://www.youtube.com/results?search_query=";
+chrome.webRequest.onBeforeRequest.addListener(
+	function(details) {
+		alert(details.url.match(/^https?:\/\/[^?]+[?][q][=]([^&]*).*/)[1]);
+		return {redirectUrl: host + details.url.match(/^https?:\/\/[^?]+[?][q][=]([^&]*).*/)[1]};
+	},
+	{
+		urls: [
+		  "*://yahoo.opera.com/search/*"
+		],
+		types: ["main_frame"]
+	},
+	["blocking"]
+);
+//http://yahoo.opera.com/search/?q={searchTerms}&fr=opera2&ei={inputEncoding}
